6a6f305bee6f5b1009a5752721679dcfb8c5efd1
Kurnia Cahya Febryanto <febryanto.2050251@mhs.its.ac.id>
2024-03-19 14:58:12 +0700

    feat: task 9 buttons & icons

--- lib/main.dart
+++ lib/main.dart
@@ -20,22 +20,42 @@ class Home extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Colors.purple[600],
       ),
-      body: SingleChildScrollView( // Untuk Scrolling
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Image.asset(
-                'assets/image-1.jpg',
-                fit: BoxFit.cover,
+     body: SingleChildScrollView( // Untuk scrolling
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start, 
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround, 
+                children: <Widget>[
+                  Expanded(
+                    child: Image.asset(
+                      'assets/image-1.jpg',
+                      fit: BoxFit.cover,
+                      height: 100, 
+                    ),
+                  ),
+                  SizedBox(width: 10), 
+                  Expanded(
+                    child: Image.network(
+                      'https://images.unsplash.com/photo-1582769923195-c6e60dc1d8dc?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90oy1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                      fit: BoxFit.cover,
+                      height: 100,
+                    ),
+                  ),
+                ],
               ),
-              SizedBox(height: 20),
-              Image.network(
-                'https://images.unsplash.com/photo-1582769923195-c6e60dc1d8dc?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-                fit: BoxFit.cover,
-              ),
-            ],
-          ),
+            ),
+            IconButton(
+              onPressed: () {
+                print('IconButton clicked');
+              },
+              icon: Icon(Icons.alternate_email),
+              color: Colors.amber,
+              iconSize: 50.0, // Adjust the icon size as needed
+            ),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
