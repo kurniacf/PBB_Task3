ef5d1ce34002e855eed86d7d6be7912462fe83c4
Kurnia Cahya Febryanto <febryanto.2050251@mhs.its.ac.id>
2024-03-22 21:57:53 +0700

    feat: personal card project

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() => runApp(MaterialApp(
   theme: ThemeData(
     primarySwatch: Colors.purple,
+    appBarTheme: AppBarTheme(
+      foregroundColor: Colors.white, 
+    ),
     textTheme: GoogleFonts.poppinsTextTheme(
       ThemeData.light().textTheme,
     ),
@@ -12,56 +17,90 @@ void main() => runApp(MaterialApp(
 ));
 
 class Home extends StatelessWidget {
+  // Fungsi untuk membuka URL
+  Future<void> _launchURL(Uri url) async {
+    if (!await launchUrl(url, mode: LaunchMode.externalApplication)) {
+      throw 'Could not launch $url';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: Text('ProjectQ'),
-          centerTitle: true,
-          backgroundColor: Colors.purple[600],
+        title: Text('Personal Card ProjectQ'),
+        centerTitle: true,
+        backgroundColor: Colors.purple[600],
       ),
-     body: SingleChildScrollView( // Untuk scrolling
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start, 
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround, 
+
+      // SingleChildScrollView agar bisa di-scroll
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Container(
+              padding: EdgeInsets.all(10.0),
+              margin: EdgeInsets.all(20.0),
+              color: Colors.purple[50],
+              child: Column(
                 children: <Widget>[
-                  Expanded(
-                    child: Image.asset(
-                      'assets/image-1.jpg',
-                      fit: BoxFit.cover,
-                      height: 100, 
+                  CircleAvatar(
+                    radius: 50.0,
+                    backgroundImage: AssetImage('assets/image-1.jpg'),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Kurnia Cahya Febryanto',
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      color: Colors.purple[800],
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                  SizedBox(width: 10), 
-                  Expanded(
-                    child: Image.network(
-                      'https://images.unsplash.com/photo-1582769923195-c6e60dc1d8dc?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90oy1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-                      fit: BoxFit.cover,
-                      height: 100,
+                  SizedBox(height: 5),
+                  Text(
+                    'Student',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.purple[600],
+                      letterSpacing: 2.5,
                     ),
                   ),
+                  SizedBox(height: 10),
+                  Text(
+                    'kcf170202@gmail.com',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.purple[600],
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      IconButton(
+                        onPressed: () => _launchURL(Uri.parse("https://www.instagram.com/kurniacf/")),
+                        icon: Icon(FontAwesomeIcons.instagram),
+                        color: Colors.purple[800],
+                      ),
+                      IconButton(
+                        onPressed: () => _launchURL(Uri.parse("https://github.com/kurniacf")),
+                        icon: Icon(FontAwesomeIcons.github),
+                        color: Colors.purple[800],
+                      ),
+                      IconButton(
+                        onPressed: () => _launchURL(Uri.parse("https://www.linkedin.com/in/kurnia-cahya-febryanto/")),
+                        icon: Icon(FontAwesomeIcons.linkedin),
+                        color: Colors.purple[800],
+                      ),
+                    ],
+                  ),
+              
                 ],
               ),
             ),
-            IconButton(
-              onPressed: () {
-                print('IconButton clicked');
-              },
-              icon: Icon(Icons.alternate_email),
-              color: Colors.amber,
-              iconSize: 50.0, // Adjust the icon size as needed
-            ),
-          ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: Colors.purple[800], 
-        child: Icon(Icons.add),
       ),
     );
   }
